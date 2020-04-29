89e73ce21ab6321e44cbce51d115e5b3bb28dfd4
Kevin Strathdee <strathdee.kevin@gmail.com>
2020-04-29 19:54:22 +0200

    implemented user endpoint

---
+++ Beanies-Backend/Users.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+        id : 1,
+        name: 'Kevin',
+        played: 5,
+        won: 2
+    },
+    {
+        id : 2,
+        name: 'Caro',
+        played: 8,
+        won: 6
+    },
+    {
+        id : 3,
+        name: 'Becca',
+        played: 15,
+        won: 7
+    }
+]
+module.exports= users;
--- Beanies-Backend/index.js
+++ Beanies-Backend/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const logger = require("./middleware/logger");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send("Hello World!"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT , () => console.log(`Server Started on Port ${PORT}`));
+// init middleware
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// user api routes
+app.use('/api/user', require('./routes/api/user'));
+
+
+app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
