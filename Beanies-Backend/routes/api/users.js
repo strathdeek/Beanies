dcf1103134d8386b8664b9f42846918daa67d970
Kevin Strathdee <strathdee.kevin@gmail.com>
2020-05-02 11:25:39 +0200

    added guest field for user added put for user added post, get, put, for guest added create user from guest

--- Beanies-Backend/routes/api/users.js
+++ Beanies-Backend/routes/api/users.js
@@ -48,6 +48,10 @@ router.post("/login", (req, res, next) => {
 router.post("/register", (req, res, next) => {
   const { name, email, password } = req.body;
   let errors = [];
+  if (!name || !email || !password) {
+    errors.push({ msg: "Must provide email, password, and name" });
+    res.status(401).json({ success: false, errors: errors });
+  }
 
   User.findOne({ email: email }).then((user) => {
     if (user) {
@@ -60,6 +64,7 @@ router.post("/register", (req, res, next) => {
         name,
         email,
         password,
+        guest: false,
       });
 
       // Hash password
@@ -86,10 +91,189 @@ router.post("/register", (req, res, next) => {
   });
 });
 
-// protected
-router.get("/protected", passport.authenticate('jwt', {session: false}), (req, res, next) => {
-    res.status(200).json({success: true, msg: "You are authorized"});
+// register guest
+router.post("/guest", (req, res, next) => {
+  const { name } = req.body;
+  const newUser = new User({
+    name: name,
+    guest: true,
+  });
+  newUser
+    .save()
+    .then((user) => {
+      res.status(200).json({
+        success: true,
+        user: user,
+      });
+    })
+    .catch((err) => next(err));
 });
+
+// register user from guest
+router.post("/register/:id", (req, res, next) => {
+  const { name, email, password } = req.body;
+  let errors = [];
+  if (!email || !password) {
+    errors.push({ msg: "Must provide email, and password" });
+    res.status(401).json({ success: false, errors: errors });
+  }
+
+  User.findOne({ email: email }).then((user) => {
+    if (user) {
+      errors.push({
+        msg: "User email is already registered",
+      });
+      res.status(401).json({ success: false, errors: errors });
+    } else {
+      User.findOne({ _id: req.params.id })
+        .then((userToUpdate) => {
+          if (!userToUpdate) {
+            res
+              .status(401)
+              .json({ success: false, msg: "Could not find user" });
+          }
+          if (name && name !== userToUpdate.name) {
+            userToUpdate.name = name;
+          }
+          userToUpdate.email = email;
+          userToUpdate.guest = false;
+          // Hash password
+          bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) throw err;
+              userToUpdate.password = hash;
+            })
+          );
+          userToUpdate
+            .save()
+            .then((user) => {
+              const jwt = utils.issueJWT(user);
+              res.status(200).json({
+                success: true,
+                user: user,
+                token: jwt.token,
+                expiresIn: jwt.expires,
+              });
+            })
+            .catch((err) => next(err));
+        })
+        .catch((err) => {
+          res.status(401).json({ success: false, msg: "Could not find user" });
+          next(err);
+        });
+    }
+  });
+});
+
+// get self
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    res
+      .status(200)
+      .json({ success: true, msg: "You are authorized", user: req.user });
+  }
+);
+
+//get by id
+router.get("/:id", (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .then((user) => {
+      if (!user) {
+        res.status(401).json({ success: false, msg: "Could not find user" });
+      }
+      res.status(200).json({
+        success: true,
+        user: {
+          _id: user._id,
+          name: user.name,
+          date: user.date,
+          guest: user.guest,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(401).json({ success: false, msg: "Could not find user" });
+      next(err);
+    });
+});
+
+//put self
+router.put(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    const { name, email, password } = req.body;
+    User.findOne({ _id: req.user._id })
+      .then((userToUpdate) => {
+        if (name && name !== userToUpdate.name) {
+          userToUpdate.name = name;
+        }
+        if (email && email !== userToUpdate.email) {
+          userToUpdate.email = email;
+        }
+        bcrypt.compare(password, userToUpdate.password).then((match) => {
+          if (match !== true) {
+            // Hash password
+            bcrypt.genSalt(10, (err, salt) =>
+              bcrypt.hash(password, salt, (err, hash) => {
+                if (err) throw err;
+                userToUpdate.password = hash;
+              })
+            );
+          }
+        });
+        userToUpdate
+          .save()
+          .then((user) => {
+            res.status(200).json({
+              success: true,
+              user: user,
+            });
+          })
+          .catch((err) => next(err));
+      })
+      .catch((err) => next(err));
+  }
+);
+
+// put guest
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    const { name } = req.body;
+    User.findOne({ _id: req.params.id })
+      .then((user) => {
+        if (!user) {
+          res.status(401).json({ success: false, msg: "Could not find user" });
+        }
+        if (user.guest === false) {
+          res.status(401).json({
+            success: false,
+            msg: "Update only possible on guest accounts",
+          });
+        }
+        if (name && name !== user.name) {
+        }
+        user.name = name;
+        user
+          .save()
+          .then((user) => {
+            res.status(200).json({
+              success: true,
+              user: user,
+            });
+          })
+          .catch((err) => next(err));
+      })
+      .catch((err) => {
+        res.status(401).json({ success: false, msg: "Could not find user" });
+        next(err);
+      });
+  }
+);
 
 // export
 module.exports = router;
