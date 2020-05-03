50d04279980dde82aee05e9b291160ad37016c62
Kevin Strathdee <strathdee.kevin@gmail.com>
2020-05-03 14:02:27 +0200

    Get Game by id implementation

--- Beanies-Backend/routes/api/games.js
+++ Beanies-Backend/routes/api/games.js
@@ -19,39 +19,54 @@ router.post("/", async (req, res, next) => {
 
   var invalidUsers = [];
   for (let index = 0; index < players.length; index++) {
-      const element = players[index];
-      await User.findOne({ _id: element })
+    const element = players[index];
+    await User.findOne({ _id: element })
       .then((user) => {
         if (!user) {
-            // check for invalid users
-            invalidUsers.push(element);
+          // check for invalid users
+          invalidUsers.push(element);
         } else {
-            // initialize scoreboard
+          // initialize scoreboard
           scores.set(element, []);
         }
       })
       .catch((err) => next(err));
   }
   if (invalidUsers.length > 0) {
-    res.status(401).json({ success: false, msg: `no user found with id: ${invalidUsers}` });
-  }
-  else{
+    res
+      .status(401)
+      .json({ success: false, msg: `no user found with id: ${invalidUsers}` });
+  } else {
     const newGame = new Game({
-        name: name,
-        scores: scores,
-      });
-      newGame
-        .save()
-        .then((game) => {
-          res.status(200).json({ success: true, game: game });
-        })
-        .catch((err) => next(err));
+      name: name,
+      scores: scores,
+    });
+    newGame
+      .save()
+      .then((game) => {
+        res.status(200).json({ success: true, game: game });
+      })
+      .catch((err) => next(err));
   }
 });
 
 // Get by game id
 router.get("/:id", (req, res, next) => {
-  // todo
+  Game.findOne({ _id: req.params.id })
+    .then((game) => {
+      if (!game) {
+          res.status(404).json({success:false,msg:`unable to find game with id: ${req.params.id}`});
+      } else {
+        res.status(200).json({
+          success: true,
+          game: game,
+        });
+      }
+    })
+    .catch((err) => {
+        res.status(404).json({success:false,msg:`unable to find game with id: ${req.params.id}`});
+        next(err);
+    });
 });
 
 // get all self
