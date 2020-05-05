c07319a0cfe4d3083ba6436b69f31e0d5c446a09
Kevin Strathdee <strathdee.kevin@gmail.com>
2020-05-05 21:02:31 +0200

    changed bcrypt version

--- Beanies-Backend/routes/api/users.js
+++ Beanies-Backend/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const utils = require("../../lib/utils");
 
 // User model
