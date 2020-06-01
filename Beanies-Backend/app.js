32679363f7e5e0a4713fb62d97db2d4b56c8ad1b
Kevin Strathdee <kstrath818@gmail.com>
2020-06-01 16:40:08 +0200

    more domain changes

--- Beanies-Backend/app.js
+++ Beanies-Backend/app.js
@@ -65,7 +65,7 @@ app.use('/api/games', require('./routes/api/games'));
 //Certificate
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/strathdeek.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/strathdeek.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/strathdeek.com/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
