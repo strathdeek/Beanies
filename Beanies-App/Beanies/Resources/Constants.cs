1ff9dfa83b5bedd8f6ba66db5db6e8e8ddfc4626
Kevin Strathdee <kstrath818@gmail.com>
2020-06-27 15:41:53 +0200

    updated constants to support multiple Databases

--- Beanies-App/Beanies/Resources/Constants.cs
+++ Beanies-App/Beanies/Resources/Constants.cs
@@ -5,7 +5,10 @@ namespace Beanies.Resources
 {
     public static class Constants
     {
-        public const string DatabaseFilename = "UserDatabase.db3";
+        private static string BasePath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public const string UserDatabaseFilename = "UserDatabase.db3";
+        public const string GameDatabaseFilename = "GameDatabase.db3";
 
         public const SQLite.SQLiteOpenFlags Flags =
             // open the database in read/write mode
@@ -15,12 +18,19 @@ namespace Beanies.Resources
             // enable multi-threaded database access
             SQLite.SQLiteOpenFlags.SharedCache;
 
-        public static string DatabasePath
+        public static string UserDatabasePath
         {
             get
             {
-                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                return Path.Combine(basePath, DatabaseFilename);
+                return Path.Combine(BasePath, UserDatabaseFilename);
+            }
+        }
+
+        public static string GameDatabasePath
+        {
+            get
+            {
+                return Path.Combine(BasePath, GameDatabaseFilename);
             }
         }
     }
