6c7f0d5badcbbf29888f20c2b455072291bd1a48
Kevin Strathdee <kstrath818@gmail.com>
2020-06-27 12:16:02 +0200

    Save session data to app props

--- Beanies-App/Beanies/Services/Backend/Interfaces/ISessionService.cs
+++ Beanies-App/Beanies/Services/Backend/Interfaces/ISessionService.cs
@@ -8,8 +8,9 @@ namespace Beanies.Services.Backend.Interfaces
     interface ISessionService
     {
         string Token { get; set; }
-
         DateTime TokenExpiration { get; set; }
         User Self { get; set; }
+
+        bool HasActiveSession();
     }
 }
--- Beanies-App/Beanies/Services/Backend/SessionService.cs
+++ Beanies-App/Beanies/Services/Backend/SessionService.cs
@@ -3,13 +3,52 @@ using Beanies.Services.Backend.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Beanies.Services.Backend
 {
     class SessionService : ISessionService
     {
+        private string tokenKey = nameof(Token);
+        private string tokenExpirationKey = nameof(TokenExpiration);
+
         public string Token { get; set; }
         public DateTime TokenExpiration { get; set; }
         public User Self { get; set; }
+
+        public bool HasActiveSession()
+        {
+            bool hasToken = !string.IsNullOrEmpty(Token);
+            bool tokenValid = (TokenExpiration!=null) && TokenExpiration > DateTime.Now;
+
+            if (hasToken && tokenValid)
+            {
+                if (Self == null)
+                    UpdateCurrentUserAsync();
+                return true;
+            }
+            return false;
+        }
+
+        private async Task UpdateCurrentUserAsync()
+        {
+            var userService = DependencyService.Resolve<IUserBackendService>();
+
+            var currentUser = await userService.GetSelfAsync();
+            Self = currentUser;
+        }
+
+        private void FetchSessionData()
+        {
+            Token = (string)Application.Current.Properties[tokenKey];
+            TokenExpiration = (DateTime)Application.Current.Properties[tokenExpirationKey];
+        }
+
+        private void SaveSessionData()
+        {
+            Application.Current.Properties[tokenKey] = Token;
+            Application.Current.Properties[tokenExpirationKey] = TokenExpiration;
+        }
     }
 }
